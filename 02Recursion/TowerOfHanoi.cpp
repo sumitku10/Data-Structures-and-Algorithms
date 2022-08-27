2081a506e789af31d4743c5fa3365d1c8de815c4
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-08-27 23:24:42 +0530

    new

--- 02Recursion/TowerOfHanoi.cpp
+++ 02Recursion/TowerOfHanoi.cpp
@@ -3,12 +3,12 @@ using namespace std;
 void toh(int n,int a,int b,int c){
     if(n>0){
         toh(n-1,a,c,b);
-        cout<<"move"<<a<<c;
+        cout<<" move \n "<<a<<c;
         toh(n-1,b,a,c);
     }
 };
 int main(){
-    toh(3,1,2,3);
+    toh(4,1,2,3);
 
  return(0);
 }
