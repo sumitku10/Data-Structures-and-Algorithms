518367e0d3d1b7e6a3e41457616185e54133eb8d
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-08-28 19:24:15 +0530

    new

---
+++ 03Arrays/Array_sizeIncrease.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+    int *p,*q;
+    int i;
+    p=(int*)malloc(5*sizeof(int));
+    p[0]=3; p[1]=5; p[2]=7; p[3]=9; p[4]=11;
+
+
+    q=(int*)malloc(10*sizeof(int));
+     for(i=0;i<5;i++){
+        q[i]=p[i]; 
+    }
+    // free(p);
+    // p=q;
+    // q=NULL;
+
+    for(i=0;i<5;i++)
+        cout<<q[i]<<endl; 
+    
+
+ return(0);
+}
