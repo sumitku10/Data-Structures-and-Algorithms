838074e447537e7437810d462f58999c77317c87
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-08-30 22:09:41 +0530

    new

--- 03Arrays/Array_sizeIncrease.cpp
+++ 03Arrays/Array_sizeIncrease.cpp
@@ -6,10 +6,11 @@ using namespace std;
 int main(){
     int *p,*q;
     int i;
+
     p=(int*)malloc(5*sizeof(int));
     p[0]=3; p[1]=5; p[2]=7; p[3]=9; p[4]=11;
 
-
+// increase size of array
     q=(int*)malloc(10*sizeof(int));
      for(i=0;i<5;i++){
         q[i]=p[i]; 
@@ -17,7 +18,7 @@ int main(){
     // free(p);
     // p=q;
     // q=NULL;
-
+// print the array //
     for(i=0;i<5;i++)
         cout<<q[i]<<endl; 
     
