92d132ed46e3892ade2cd835e2363f7358e6ba69
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-11-12 00:40:10 +0530

    new

--- 03Arrays/Array_Declaration.cpp
+++ 03Arrays/Array_Declaration.cpp
@@ -4,10 +4,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a[5]; // array size
-    int i;
-    for(i=0;i<5;i++){
-       cout<<a[i]<<endl;
-    }
- return(0);
+   int a[5]; // array size
+   int i;
+   for(i=0;i<5;i++){
+      cout<<a[i]<<endl;
+   }
+ return 0;
 }
